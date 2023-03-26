7df4adce75b76351ee2cea93c872e831c01606cf
Wallace-Bezerra <89711908+Wallace-Bezerra@users.noreply.github.com>
2023-03-26 20:38:27 -0300

    feat: ready visual structure

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,14 +1,21 @@
 import * as Dialog from "@radix-ui/react-dialog";
-import { Content, FormModal, Overlay } from "./styles";
+import {
+  ButtonTransaction,
+  ButtonTransactionContainer,
+  Content,
+  FormModal,
+  Overlay,
+} from "./styles";
+import ArrowUp from "../../assets/ArrowUp.svg";
+import ArrowDown from "../../assets/ArrowDown.svg";
 import Close from "../../assets/Close.svg";
 
 export const NewTransactionModal = () => {
   return (
     <Dialog.Portal>
       <Overlay
-        initial={{ opacity: 0, translateY: -30 }}
+        initial={{ opacity: 0 }}
         animate={{
-          translateY: 0,
           opacity: 1,
           transition: { type: "tween", duration: 0.3 },
         }}
@@ -22,14 +29,72 @@ export const NewTransactionModal = () => {
         >
           <Dialog.Title>Nova transação</Dialog.Title>
           <Dialog.Close asChild>
-            <img src={Close} alt="" />
+            <img className="close" src={Close} alt="" />
           </Dialog.Close>
           <FormModal>
             <input type="text" placeholder="Descrição" required />
             <input type="number" placeholder="Preço" required />
             <input type="text" placeholder="Categoria" required />
-            {/* <div>Entrada</div>
-            <div>Saida</div> */}
+            <ButtonTransactionContainer>
+              <ButtonTransaction value="entry" variant="entry">
+                <svg
+                  width="25"
+                  height="24"
+                  viewBox="0 0 25 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    clip-rule="evenodd"
+                    d="M12.25 3.75012C7.69365 3.75012 4 7.44377 4 12.0001C4 16.5565 7.69365 20.2501 12.25 20.2501C16.8063 20.2501 20.5 16.5565 20.5 12.0001C20.5 7.44377 16.8063 3.75012 12.25 3.75012ZM2.5 12.0001C2.5 6.61535 6.86522 2.25012 12.25 2.25012C17.6348 2.25012 22 6.61535 22 12.0001C22 17.3849 17.6348 21.7501 12.25 21.7501C6.86522 21.7501 2.5 17.3849 2.5 12.0001Z"
+                    fill="white"
+                  />
+                  <path
+                    fill-rule="evenodd"
+                    clip-rule="evenodd"
+                    d="M12.25 7.50012C12.4489 7.50012 12.6396 7.57914 12.7803 7.71979L15.9584 10.8979C16.2513 11.1908 16.2513 11.6657 15.9584 11.9586C15.6655 12.2515 15.1907 12.2515 14.8978 11.9586L12.25 9.31078L9.60217 11.9586C9.30928 12.2515 8.8344 12.2515 8.54151 11.9586C8.24862 11.6657 8.24862 11.1908 8.54151 10.8979L11.7196 7.71979C11.8603 7.57914 12.0511 7.50012 12.25 7.50012Z"
+                    fill="white"
+                  />
+                  <path
+                    fill-rule="evenodd"
+                    clip-rule="evenodd"
+                    d="M12.25 7.50012C12.6642 7.50012 13 7.83591 13 8.25012L13 15.7501C13 16.1643 12.6642 16.5001 12.25 16.5001C11.8358 16.5001 11.5 16.1643 11.5 15.7501L11.5 8.25012C11.5 7.83591 11.8358 7.50012 12.25 7.50012Z"
+                    fill="white"
+                  />
+                </svg>
+                <span>Entrada</span>
+              </ButtonTransaction>
+              <ButtonTransaction value="output" variant="output">
+                <svg
+                  width="25"
+                  height="24"
+                  viewBox="0 0 25 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    clip-rule="evenodd"
+                    d="M12.25 3.75012C7.69365 3.75012 4 7.44377 4 12.0001C4 16.5565 7.69365 20.2501 12.25 20.2501C16.8063 20.2501 20.5 16.5565 20.5 12.0001C20.5 7.44377 16.8063 3.75012 12.25 3.75012ZM2.5 12.0001C2.5 6.61535 6.86522 2.25012 12.25 2.25012C17.6348 2.25012 22 6.61535 22 12.0001C22 17.3849 17.6348 21.7501 12.25 21.7501C6.86522 21.7501 2.5 17.3849 2.5 12.0001Z"
+                    fill="white"
+                  />
+                  <path
+                    fill-rule="evenodd"
+                    clip-rule="evenodd"
+                    d="M12.25 7.50012C12.4489 7.50012 12.6396 7.57914 12.7803 7.71979L15.9584 10.8979C16.2513 11.1908 16.2513 11.6657 15.9584 11.9586C15.6655 12.2515 15.1907 12.2515 14.8978 11.9586L12.25 9.31078L9.60217 11.9586C9.30928 12.2515 8.8344 12.2515 8.54151 11.9586C8.24862 11.6657 8.24862 11.1908 8.54151 10.8979L11.7196 7.71979C11.8603 7.57914 12.0511 7.50012 12.25 7.50012Z"
+                    fill="white"
+                  />
+                  <path
+                    fill-rule="evenodd"
+                    clip-rule="evenodd"
+                    d="M12.25 7.50012C12.6642 7.50012 13 7.83591 13 8.25012L13 15.7501C13 16.1643 12.6642 16.5001 12.25 16.5001C11.8358 16.5001 11.5 16.1643 11.5 15.7501L11.5 8.25012C11.5 7.83591 11.8358 7.50012 12.25 7.50012Z"
+                    fill="white"
+                  />
+                </svg>
+                <span>Saida</span>
+              </ButtonTransaction>
+            </ButtonTransactionContainer>
             <button type="submit">Cadastrar</button>
           </FormModal>
         </Content>
--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import { motion } from "framer-motion";
 import * as Dialog from "@radix-ui/react-dialog";
+import * as RadioGroup from "@radix-ui/react-radio-group";
 
 export const Overlay = styled(motion(Dialog.Overlay))`
   background-color: #000000bf;
@@ -24,7 +25,7 @@ export const Content = styled(motion(Dialog.Content))`
   h2 {
     margin-bottom: 32px;
   }
-  img {
+  .close {
     cursor: pointer;
     position: absolute;
     top: 24px;
@@ -40,7 +41,13 @@ export const FormModal = styled.form`
     padding: 16px;
     border-radius: 6px;
     background-color: ${({ theme }) => theme["gray-900"]};
-    color: ${({ theme }) => theme["gray-500"]};
+    color: ${({ theme }) => theme.white};
+    ::placeholder {
+      font-weight: 400;
+      font-size: 16px;
+      line-height: 140%;
+      color: ${({ theme }) => theme["gray-500"]};
+    }
   }
   button[type="submit"] {
     padding: 16px 32px;
@@ -50,5 +57,53 @@ export const FormModal = styled.form`
     font-weight: 700;
     font-size: 16px;
     line-height: 160%;
+  }
+`;
+export const ButtonTransactionContainer = styled(RadioGroup.Root)`
+  display: flex;
+  gap: 16px;
+  margin-bottom:30px;
+`;
+interface ButtonTransactionProps {
+  variant: "entry" | "output";
+}
+export const ButtonTransaction = styled(RadioGroup.Item)<ButtonTransactionProps>`
+  padding: 16px 24px;
+  background: #29292e;
+  border-radius: 6px;
+  width: 100%;
+  display: flex;
+  gap: 8px;
+  justify-content: center;
+  align-items: center;
+  transition: background-color .3s cubic-bezier(0.19, 1, 0.22, 1);
+  svg {
+    path {
+      fill: ${({ theme, variant }) =>
+        variant === "entry" ? theme["green-300"] : theme["red-300"]};
+    }
+  }
+  span {
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 160%;
+    color: ${({ theme }) => theme["gray-300"]};
+  }
+
+  &[data-state="checked"] {
+    background: ${({ theme, variant }) =>
+      variant === "entry" ? theme["green-700"] : theme["red-500"]};
+    span {
+      color: white;
+    }
+    svg {
+      path {
+        fill: white;
+      }
+    }
+  }
+  &[data-state="unchecked"]:hover{
+    background: ${({ theme}) =>
+       theme["gray-600"]};
   }
 `;
